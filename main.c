5989ba13b95e71b93c6cbc44a5ad1cf0d28ac2fd
Dan Santos <ivan.santos.v@mail.pucv.cl>
2019-08-22 20:23:22 -0400

    Se implementa funcion evaluarEquilibrio

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "TDA-Pila/Stack.h"
 
@@ -22,6 +23,7 @@ typedef struct Movil{
 }Movil;
 
 /** --------- Prototipos --------- */
+bool evaluarEquilibrio(Movil *nodo_inicial);
 void leerInfo(Movil *nodo_actual);
 Movil *crearNodo(void);
 int pesoTotal(Movil *nodo_act);
@@ -32,7 +34,35 @@ int main()
 
     leerInfo(nodo_inicial);
 
+    if(evaluarEquilibrio(nodo_inicial) == true)
+        printf("SI");
+    else
+        printf("NO");
+
     return 0;
+}
+
+/** Funcion que evalua si el movil ingresado esta en equilibrio, si lo esta retorna true, sino false */
+bool evaluarEquilibrio(Movil *nodo_inicial)
+{
+    Stack *pila = createStack();
+    push(pila, nodo_inicial);
+
+    while(stackCount(pila) != 0)
+    {
+        Movil *nodo_act = pop(pila);
+
+        if((nodo_act->peso_izq * nodo_act->dist_izq) != (nodo_act->peso_der * nodo_act->dist_der))
+            return false;
+
+        if(nodo_act->left != NULL)
+            push(pila, nodo_act->left);
+
+        if(nodo_act->right != NULL)
+            push(pila, nodo_act->right);
+    }
+
+    return true;
 }
 
 /** Procedimiento recursivo, que va leyendo la informacion de los nodos del movil. */
